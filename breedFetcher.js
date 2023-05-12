311c49ba04001218132a37ac94ff12e22207bea5
Arian <arianahmadi1@gmail.com>
2023-05-12 05:38:35 -0400

    update and refactor plus added test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,18 @@
 const request = require('request');
 
-const breedName = process.argv[2];
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+const fetchBreedDescription = (breedName, callback) => {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request.get(url, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      return callback('Breed not found', null);
+    }
+    callback(null, data[0].description);
+  });
+};
 
-request.get(url, 'utf8', (error, response, body) => {
-  if (error) {
-    console.log(error);
-  }
-  if (response.statusCode !== 200) {
-    console.log(`Error ${response.statusCode}`);
-    return;
-  }
-
-  const data = JSON.parse(body);
-  // console.log(data.length);
-  // check the length of data
-  if (data.length === 0) {
-    console.log(`No results for ${breedName}`);
-    return;
-  }
-  console.log(data[0].description);
-});
+module.exports = { fetchBreedDescription };
